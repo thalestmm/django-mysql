2cc57beaf7ad3529d3d5048dca11ab5d7e3ac53c
thalestmm <meier.thales@gmail.com>
2023-04-24 08:49:04 -0300

    model updates

--- content/models.py
+++ content/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
 
 class Instructor(models.Model):
     name = models.CharField(max_length=50)
     country = models.CharField(max_length=30)
     email = models.EmailField(null=True, blank=True)
     bio = models.TextField(max_length=500, null=True, blank=True)
+    profile_photo = models.ImageField(null=True, blank=True) # TODO: Implement static file upload and serving
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name 
+
 
 # Abstract model for all things related to content
 class BaseContent(models.Model):
@@ -49,15 +50,26 @@ class Module(BaseContent):
     class Meta:
         ordering = ['course', 'index', 'name']
 
+
 # Specific abstract model for classes (there will be different types of classes)
 class BaseLecture(BaseContent):
     module = models.ForeignKey("Module", on_delete=models.SET_NULL, null=True)
+    lecture_text = models.TextField(max_length=1000, blank=True, null=True) # TODO: Implement markdown formatting
 
     class Meta:
         abstract = True
         ordering = ['module', 'index', 'name']
 
 
+# TODO: Add specific icons for each lecture type
 class VideoLecture(BaseLecture):
     video_url = models.URLField(null=True) # TODO: Choose proper video delivery method
 
+
+class TextLecture(BaseLecture):
+    pass
+
+
+# TODO: Add alternatives and explanations
+class Question(BaseLecture):
+    pass
